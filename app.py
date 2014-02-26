30bf8752aa272878d38795fb3c9bb2e8129d015b
Jona Haga-Camacho <jona.haga@gmail.com>
2014-02-25 23:33:31 -0800

    Finished tasks 1-3

--- app.py
+++ app.py
@@ -6,15 +6,54 @@ app.secret_key = "shhhhthisisasecret"
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    if session.get("username"):
+        return "User %s is logged in!" %session['username']
+    else:
+        return render_template("index.html")
 
 @app.route("/", methods=["POST"])
 def process_login():
-    return render_template("login.html")
+    username = request.form.get("username")
+    password = request.form.get("password")
 
-@app.route("/youshouldprobablychangethisurl")
+    username = model.authenticate(username, password)
+    if username != None:
+        flash("User authenticated!")
+        session['username'] = username
+    else:
+        flash("Password incorrect, there may be a ferret stampede in progress!")
+
+    return redirect(url_for("view_user", username=request.form.get("username")))
+
+@app.route("/register")
 def register():
+    if session.get("username"):
+        username = model.get_username_by_id(session.get("username"))
+        print username
+        return redirect(url_for('view_user', username=username))
     return render_template("register.html")
+
+@app.route("/")
+def session_clear():
+    session.clear()
+    return render_template("index.html")
+
+@app.route("/user/<username>")
+def view_user(username):
+    model.connect_to_db()
+    user_id = model.get_userid_by_name(username)
+    wall_posts = model.get_wall_posts_by_user(user_id)
+    return render_template("wall.html", wall_posts = wall_posts, username = username, session = session)
+
+@app.route("/wall/<username>", methods=["POST"])
+def post_to_wall(username):
+    model.connect_to_db()
+    user_id = model.get_userid_by_name(username)
+    post_input = request.form.get("content")
+    author_id = session['username']
+    model.create_wall_post(user_id, author_id, post_input)
+    return redirect(url_for("view_user", username=username))
+    
 
 if __name__ == "__main__":
     app.run(debug = True)
--- model.py
+++ model.py
@@ -1,5 +1,63 @@
-ADMIN_USER="hackbright"
-ADMIN_PASSWORD=803096023
+import sqlite3
+
+DB = None
+CONN = None
+
+def connect_to_db():
+    global DB, CONN
+    # Checks to see if connection is already established
+    if DB == None:
+        CONN = sqlite3.connect("thewall.db")
+        DB = CONN.cursor()
 
 def authenticate(username, password):
-    return False
+    connect_to_db()
+    query = """SELECT username, password, id 
+               FROM users 
+               WHERE username = ?"""
+    DB.execute(query, (username,))
+    row = DB.fetchone()
+    if username == row[0] and hash(password) == int(row[1]):
+        return row[2] # userid
+    else:
+        return None
+
+def get_userid_by_name(username):
+    connect_to_db()
+    query = """SELECT id 
+               FROM users 
+               WHERE username = ?"""
+    DB.execute(query, (username,))
+    row = DB.fetchone()
+    return row[0] # userid
+
+def get_username_by_id(user_id):
+    connect_to_db()
+    query = """ SELECT username
+                FROM users
+                WHERE id = ?"""
+    DB.execute(query, (user_id,))
+    row = DB.fetchone()
+    return str(row[0]) # username
+
+def get_wall_posts_by_user(user_id):
+    connect_to_db()
+    query = """SELECT users.username, content, created_at 
+               FROM wall_posts 
+               JOIN users ON (users.id=wall_posts.author_id) 
+               WHERE owner_id=?"""
+    DB.execute(query, (user_id,))
+    row = DB.fetchall()
+    return row
+
+def create_wall_post(owner_id, author_id, content):
+    connect_to_db()
+    query = """INSERT INTO wall_posts (owner_id, author_id, created_at, content) 
+               VALUES (?, ?, datetime('now'), ?)"""
+    print "This is the query %r" %(query)
+    DB.execute(query, (owner_id, author_id, content))
+    CONN.commit()            
+
+    # author_id being the user_id associated with the username
+
+
